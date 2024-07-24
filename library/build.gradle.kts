7a92fdd7f28257748f7bd3cc48f0ccac84fb0d74
gerwalex <gerwalex@gmail.com>
2024-07-24 10:00:40 +0200

    Initial commit

---
+++ library/build.gradle.kts
@@ -0,0 +1,87 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.compose.compiler)
+    id("maven-publish")
+}
+
+android {
+    namespace = "com.gerwalex.library"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+        // Compile für unchecked und deprecation
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+        freeCompilerArgs = listOf(
+            "-Xjavac-arguments='-Xlint:unchecked -Xlint:deprecation'",
+            "-opt-in=kotlin.RequiresOptIn"
+        )
+    }
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
+
+    buildFeatures {
+        compose = true
+        buildConfig = true
+    }
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+            withJavadocJar()
+        }
+    }
+}
+
+dependencies {
+    implementation(libs.google.play.ads)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+}
+
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            groupId = "github.gitwalex.com"
+            artifactId = "library"
+            version = "0.0.1"
+
+            afterEvaluate {
+                from(components["release"])
+            }
+        }
+    }
+}
---
+++ library/src/main/java/com/gerwalex/library/ads/AdHelper.kt
@@ -0,0 +1,134 @@
+package com.gerwalex.library.ads
+
+import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalInspectionMode
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import com.google.android.gms.ads.AdListener
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdSize
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.rewardedinterstitial.RewardedInterstitialAd
+import com.google.android.gms.ads.rewardedinterstitial.RewardedInterstitialAdLoadCallback
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+@Composable
+fun BannerAdView(adSize: AdSize, adUnitId: String, modifier: Modifier = Modifier) {
+    val isInEditMode = LocalInspectionMode.current
+    if (isInEditMode) {
+        Text(
+            modifier = modifier
+                .fillMaxWidth()
+                .background(Color.Red)
+                .padding(horizontal = 2.dp, vertical = 6.dp),
+            textAlign = TextAlign.Center,
+            color = Color.White,
+            text = "BannerAdView Here",
+        )
+    } else {
+        HorizontalDivider()
+        AndroidView(
+            modifier = modifier.fillMaxWidth(),
+            factory = { context ->
+                AdView(context).apply {
+                    setAdSize(adSize)
+                    this.adUnitId = adUnitId
+
+                    AdRequest.Builder().build().apply {
+                        Log.d("AdHelpder", "isTestDevice: ${this.isTestDevice(context)}")
+                        loadAd(this)
+                    }
+                }
+            }
+        )
+        HorizontalDivider()
+    }
+}
+
+@Composable
+fun BannerAdView(
+    adUnitId: String, maxHeight: Int,
+    modifier: Modifier = Modifier,
+    adListener: AdListener? = null,
+) {
+    val isInEditMode = LocalInspectionMode.current
+    if (isInEditMode) {
+        Text(
+            modifier = modifier
+                .fillMaxWidth()
+                .background(Color.Red)
+                .padding(horizontal = 2.dp, vertical = 6.dp),
+            textAlign = TextAlign.Center,
+            color = Color.White,
+            text = "BannerAdView Here",
+        )
+    } else {
+        HorizontalDivider()
+        BoxWithConstraints(
+            modifier = Modifier.fillMaxWidth(),
+            contentAlignment = Alignment.Center
+        ) {
+            val adSize = remember {
+                AdSize.getInlineAdaptiveBannerAdSize(
+                    maxWidth.value.toInt(), maxHeight
+                )
+            }
+            AndroidView(
+                modifier = modifier.fillMaxWidth(),
+                factory = { context ->
+                    AdView(context).apply {
+                        setAdSize(adSize)
+                        adListener?.let { this.adListener = it }
+                        this.adUnitId = adUnitId
+                        AdRequest.Builder().build().apply {
+                            Log.d("AdHelpder", "isTestDevice: ${this.isTestDevice(context)}")
+                            loadAd(this)
+                        }
+                    }
+                }
+            )
+        }
+        HorizontalDivider()
+    }
+}
+
+
+@Composable
+fun rememberRewardedInterstatialAd(
+    rewarded_interstitial_ad_id: String,
+    modifier: Modifier = Modifier,
+    onAdLoadAdError: (LoadAdError) -> Unit,
+): StateFlow<RewardedInterstitialAd?> {
+    val loadedAd = MutableStateFlow<RewardedInterstitialAd?>(null)
+    val context = LocalContext.current
+
+    RewardedInterstitialAd.load(
+        context,
+        rewarded_interstitial_ad_id,
+        AdRequest.Builder().build(),
+        object : RewardedInterstitialAdLoadCallback() {
+            override fun onAdLoaded(ad: RewardedInterstitialAd) {
+                loadedAd.value = ad
+            }
+
+            override fun onAdFailedToLoad(loadAdError: LoadAdError) {
+                onAdLoadAdError(loadAdError)
+            }
+        })
+    return remember { loadedAd }
+}
